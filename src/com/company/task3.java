8785efdc2b5239f7f3c19ffd3d0ba3432d12e93a
Maks162602 <100307397+Maks162602@users.noreply.github.com>
2022-04-05 11:08:14 +0300

    Add files via upload

---
+++ src/com/company/task3.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class task3 {
+    public static void main(String[] args) {
+        double a=3.35;
+        a+=0.05;
+        a+=0.6;
+        System.out.println(a);
+    }
+}
